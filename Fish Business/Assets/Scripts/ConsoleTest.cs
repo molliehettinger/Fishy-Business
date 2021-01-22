394a88eb06518097cf19bdd5f77910afa69c425c
Mollie <Mollie.Hettinger01@gmail.com>
2021-01-21 23:22:17 -0500

    The other Initial Stage
    + Added folders for organization
    + Test script added to square. Simple text printed when game began.

---
+++ Fish Business/Assets/Scripts/ConsoleTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Constole Test.");   
+    }
+}
