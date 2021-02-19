a37b61bd4c03170fdef99cb00932a4fd8851d356
Mollie <Mollie.Hettinger01@gmail.com>
2021-02-18 23:02:37 -0500

    Best Movement without hops
    + Fixed weird player controller issues.
    + Added Cinemachine
    
    To add Cinemachine go to Window > Package Manager > type in cinemachine

--- Fish Business/Assets/Scripts/Player.cs
+++ Fish Business/Assets/Scripts/Player.cs
@@ -6,15 +6,20 @@ public class Player : MonoBehaviour
 {
 
 
+    public CharacterController controller;
+    public Transform cam;
+
     public float speed = 5f;
 
-    Rigidbody player_rb;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        player_rb = GetComponent<Rigidbody>();
+      
     }//ends Start
+
 
     // Update is called once per frame
     void Update()
@@ -23,25 +28,21 @@ public class Player : MonoBehaviour
 
         float xMov = Input.GetAxisRaw("Horizontal");
         float zMov = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(xMov, 0f, zMov).normalized;
 
-        player_rb.velocity = new Vector3(xMov*speed, player_rb.velocity.y, zMov *speed) *Time.deltaTime;
 
-
-        if (Input.GetKey(KeyCode.Q))
+        if (direction.magnitude >= 0.1f)
         {
-            transform.Rotate(0, -1, 0);
-        }
-  
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(0, 1, 0);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.Space))
-        {
 
-        }
-      
     
         }//ends Update
 
